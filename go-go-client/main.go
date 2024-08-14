065a415548388f2c1bf727ce8f0cc6a4957752d3
DD <denys@MacBook-Air-DD.local>
2024-08-14 22:32:43 +0300

    Simple TCP client connection

--- go-go-client/main.go
+++ go-go-client/main.go
@@ -1,6 +1,32 @@
 package main
 
-func main() {
-	// Code goes here ...
-}
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
 
+func main() {
+	conn, err := net.Dial("tcp", "192.168.0.111:6969")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	fmt.Println("Connected")
+	fmt.Println("Enter your messages:")
+
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		username := "uncatched"
+		message := username + ":" + text
+		data := []byte(message)
+		_, err := conn.Write(data)
+		if err != nil {
+			fmt.Println("Failed to send message")
+		}
+	}
+}
